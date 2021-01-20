eb632931fdc767c89361f23bba2a06ecec60db1b
Arimodu <Samuel.Luxik03@gmail.com>
2021-01-20 08:25:33 +0100

    Data types.

--- Scripts_By_Arimodu/HelloWorld.cs
+++ Scripts_By_Arimodu/HelloWorld.cs
@@ -8,9 +8,27 @@ namespace Scripts_By_Arimodu
     {
         public static void HelloWorldMain()
         {
-            Console.WriteLine("Hello world");
-            Console.WriteLine();
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world\nNext line");
+            int numbers = 1;
+            string sentence;
+            double decimals = 2.01;
+            char keystroke;
+
+            sentence = Console.ReadLine();
+            Console.WriteLine(sentence);
+            keystroke = Console.ReadKey().KeyChar;
+            Console.WriteLine(keystroke);
+
+            int number1 = 0;
+            int number2 = 0;
+
+            number1 = Convert.ToInt32(Console.ReadLine());
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(number1 + number2);
+
+
+
         }
     }
 }
